c0065d24a9aba81f43c0e9d5a130302bdb5d4c34
Eoin <41481352+Strrobez@users.noreply.github.com>
2023-10-13 15:57:50 +0100

    init: stack size manager

---
+++ Rust.HarmonyMods/Rust.Harmony.StackSizeManager/HamonyHooks.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+
+namespace Rust.Harmony.StackSizeManager
+{
+    internal class HarmonyHooks : IHarmonyModHooks
+    {
+        public void OnLoaded(OnHarmonyModLoadedArgs args) => HarmonyConfig.LoadConfig();
+
+        public void OnUnloaded(OnHarmonyModUnloadedArgs args)
+        {
+        }
+    }
+}
---
+++ Rust.HarmonyMods/Rust.Harmony.StackSizeManager/MyPatch.cs
@@ -0,0 +1,39 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace Rust.Harmony.StackSizeManager
+{
+    [HarmonyPatch(typeof(Bootstrap), "StartupShared")]
+    internal static class Bootstrap_StartupShared_Patch
+    {
+        [HarmonyPrefix]
+        internal static bool Prefix()
+        {
+            Debug.Log("[Harmony] Loaded: StackSizeManager by Strobez");
+
+            foreach (ItemDefinition itemDef in ItemManager.itemList)
+            {
+                int stackable = itemDef.stackable;
+
+                if (HarmonyConfig.Config == null)
+                {
+                    Debug.LogError("Configuration is null");
+                    break;
+                }
+
+                if (HarmonyConfig.Config.InvididualStackSizes.TryGetValue(itemDef.shortname, out int individualStackSize))
+                    itemDef.stackable = individualStackSize;
+
+                if (HarmonyConfig.Config.IndividualItemStackMultipliers.TryGetValue(itemDef.shortname, out int multiplier))
+                    itemDef.stackable = Mathf.RoundToInt(stackable * multiplier);
+
+                if (HarmonyConfig.Config.CategoryStackMultipliers.TryGetValue(itemDef.category.ToString(),
+                        out int categoryMultiplier) &&
+                    categoryMultiplier > 1.0f)
+                    itemDef.stackable = Mathf.RoundToInt(stackable * categoryMultiplier);
+            }
+
+            return true;
+        }
+    }
+}
